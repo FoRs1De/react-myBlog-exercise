75b0b38db6db56af709d319ec70d04e09412b109
Oleksiy Prokopan <alexeyprokopan@gmail.com>
2023-07-24 14:41:58 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import './styles/App.css';
+function App() {
+  return (
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <h2>1. JavaScript</h2>
+          <div>Javascript is a good.</div>
+        </div>
+        <div className="post__btns">
+          <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
